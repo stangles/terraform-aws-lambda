7a62d9db467ecc22d0cfc3d59d127957be96cc81
Steven Angles <sangles@sonatype.com>
2019-07-31 16:39:15 -0400

    Add edgelambda.amazonaws.com as service principal for assuming lambda execution role

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ data "aws_iam_policy_document" "assume" {
 
     principals {
       type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
+      identifiers = ["lambda.amazonaws.com", "edgelambda.amazonaws.com"]
     }
   }
 }
